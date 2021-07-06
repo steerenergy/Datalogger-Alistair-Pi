996b75e30f8e1566f2b89e151aa9576411628262
Alistair-L-R <alistairlryan@outlook.com>
2021-07-06 13:29:04 +0100

    Created ADC test python file

--- adcTest.py
+++ adcTest.py
@@ -31,68 +31,66 @@ from decimal import Decimal
 import numpy as np
 import gui
 
-
-i2c = busio.I2C(board.SCL, board.SDA, frequency=1000000)
-dataRate = 860
-
-adc0 = ADS.ADS1115(i2c, address=0x48, mode=Mode.CONTINUOUS, data_rate=dataRate)
-adc1 = ADS.ADS1115(i2c, address=0x49, mode=Mode.CONTINUOUS, data_rate=dataRate)
-adc2 = ADS.ADS1115(i2c, address=0x4a, mode=Mode.CONTINUOUS, data_rate=dataRate)
-adc3 = ADS.ADS1115(i2c, address=0x4b, mode=Mode.CONTINUOUS, data_rate=dataRate)
-
-adcPinMap = {
-            "0AX": {
-                "0A0": [AnalogIn(ads=adc0, gain=1, positive_pin=ADS.P0),0],
-                "0A1": [AnalogIn(ads=adc0, gain=1, positive_pin=ADS.P1),1],
-                "0A2": [AnalogIn(ads=adc0, gain=1, positive_pin=ADS.P2),2],
-                "0A3": [AnalogIn(ads=adc0, gain=1, positive_pin=ADS.P3),3]
-            },
-            "1AX": {
-                "1A0": [AnalogIn(ads=adc1, gain=1, positive_pin=ADS.P0),4],
-                "1A1": [AnalogIn(ads=adc1, gain=1, positive_pin=ADS.P1),5],
-                "1A2": [AnalogIn(ads=adc1, gain=1, positive_pin=ADS.P2),6],
-                "1A3": [AnalogIn(ads=adc1, gain=1, positive_pin=ADS.P3),7]
-            },
-            "2AX": {
-                "2A0": [AnalogIn(ads=adc2, gain=1, positive_pin=ADS.P0),8],
-                "2A1": [AnalogIn(ads=adc2, gain=1, positive_pin=ADS.P1),9],
-                "2A2": [AnalogIn(ads=adc2, gain=1, positive_pin=ADS.P2),10],
-                "2A3": [AnalogIn(ads=adc2, gain=1, positive_pin=ADS.P3),11]
-            },
-            "3AX": {
-                "3A0": [AnalogIn(ads=adc3, gain=1, positive_pin=ADS.P0),12],
-                "3A1": [AnalogIn(ads=adc3, gain=1, positive_pin=ADS.P1),13],
-                "3A2": [AnalogIn(ads=adc3, gain=1, positive_pin=ADS.P2),14],
-                "3A3": [AnalogIn(ads=adc3, gain=1, positive_pin=ADS.P3),15]
-            }
-        }
-
 def ADCReader(pins):
-    while True:
-        for pin in pins:
-           print("{}: {}".format(pin[1],pin[0].value))
+    startTime = time.perf_counter()
+    with open("workertest.csv","w") as file:
+        worker_writer = csv.writer(file, dialect="excel", delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        while True:
+            for pin in pins:
+                timeElapsed = time.perf_counter() - startTime
+                worker_writer.writerow([timeElapsed] + [pin.value])
 
-#global adcbuffer
-#adcbuffer = [0] * 16
-for board in adcPinMap.values():
+
+def run():
+    i2c = busio.I2C(board.SCL, board.SDA, frequency=1000000)
+    dataRate = 860
+
+    adc0 = ADS.ADS1115(i2c, address=0x48, mode=Mode.CONTINUOUS, data_rate=dataRate)
+    adc1 = ADS.ADS1115(i2c, address=0x49, mode=Mode.CONTINUOUS, data_rate=dataRate)
+    adc2 = ADS.ADS1115(i2c, address=0x4a, mode=Mode.CONTINUOUS, data_rate=dataRate)
+    adc3 = ADS.ADS1115(i2c, address=0x4b, mode=Mode.CONTINUOUS, data_rate=dataRate)
+
+    adcPinMap = {
+        "0AX": {
+            "0A0": [AnalogIn(ads=adc0, gain=1, positive_pin=ADS.P0), 0],
+            "0A1": [AnalogIn(ads=adc0, gain=1, positive_pin=ADS.P1), 1],
+            "0A2": [AnalogIn(ads=adc0, gain=1, positive_pin=ADS.P2), 2],
+            "0A3": [AnalogIn(ads=adc0, gain=1, positive_pin=ADS.P3), 3]
+        },
+        "1AX": {
+            "1A0": [AnalogIn(ads=adc1, gain=1, positive_pin=ADS.P0), 4],
+            "1A1": [AnalogIn(ads=adc1, gain=1, positive_pin=ADS.P1), 5],
+            "1A2": [AnalogIn(ads=adc1, gain=1, positive_pin=ADS.P2), 6],
+            "1A3": [AnalogIn(ads=adc1, gain=1, positive_pin=ADS.P3), 7]
+        },
+        "2AX": {
+            "2A0": [AnalogIn(ads=adc2, gain=1, positive_pin=ADS.P0), 8],
+            "2A1": [AnalogIn(ads=adc2, gain=1, positive_pin=ADS.P1), 9],
+            "2A2": [AnalogIn(ads=adc2, gain=1, positive_pin=ADS.P2), 10],
+            "2A3": [AnalogIn(ads=adc2, gain=1, positive_pin=ADS.P3), 11]
+        },
+        "3AX": {
+            "3A0": [AnalogIn(ads=adc3, gain=1, positive_pin=ADS.P0), 12],
+            "3A1": [AnalogIn(ads=adc3, gain=1, positive_pin=ADS.P1), 13],
+            "3A2": [AnalogIn(ads=adc3, gain=1, positive_pin=ADS.P2), 14],
+            "3A3": [AnalogIn(ads=adc3, gain=1, positive_pin=ADS.P3), 15]
+        }
+    }
     pins = []
-    for pin in board.values():
+    for pin in adcPinMap["0AX"].values():
         pins.append(pin)
     worker = threading.Thread(target=ADCReader,args=(pins,))
     worker.daemon = True
     worker.start()
 
 
-startTime = time.perf_counter()
-countTime = startTime
-timeInterval = 0.1
+    startTime = time.perf_counter()
+    with open("workertest.csv","w") as file:
+        writer = csv.writer(file, dialect="excel", delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        while True:
+            for pin in pins:
+                timeElapsed = time.perf_counter() - startTime
+                writer.writerow([timeElapsed] + [pin.value])
 
-#with open("test.csv","w") as file:
-print("Logging started")
-#writer = csv.writer(file, dialect="excel", delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-while True:
-    while (time.perf_counter() - countTime) > timeInterval:
-        timeElapsed = time.perf_counter() - startTime
-        #writer.writerow([timeElapsed] + adcbuffer)
-        countTime = time.perf_counter()
-        print(timeElapsed)
+if __name__ == "__main__":
+    run()
