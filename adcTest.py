b15fc682c350c0d78d2c487edd7fe5fee6880722
Alistair-L-R <alistairlryan@outlook.com>
2021-07-06 12:37:41 +0100

    Created ADC test python file

--- adcTest.py
+++ adcTest.py
@@ -33,9 +33,48 @@ import gui
 
 
 i2c = busio.I2C(board.SCL, board.SDA, frequency=1000000)
-data=860
+dataRate = 860
 
 adc0 = ADS.ADS1115(i2c, address=0x48, mode=Mode.CONTINUOUS, data_rate=dataRate)
 adc1 = ADS.ADS1115(i2c, address=0x49, mode=Mode.CONTINUOUS, data_rate=dataRate)
 adc2 = ADS.ADS1115(i2c, address=0x4a, mode=Mode.CONTINUOUS, data_rate=dataRate)
 adc3 = ADS.ADS1115(i2c, address=0x4b, mode=Mode.CONTINUOUS, data_rate=dataRate)
+
+adcPinMap = {
+            "0AX": {
+                "0A0": [AnalogIn(ads=adc0, gain=1, positive_pin=ADS.P0)],
+                "0A1": [AnalogIn(ads=adc0, gain=1, positive_pin=ADS.P1)],
+                "0A2": [AnalogIn(ads=adc0, gain=1, positive_pin=ADS.P2)],
+                "0A3": [AnalogIn(ads=adc0, gain=1, positive_pin=ADS.P3)]
+            },
+            "1AX": {
+                "1A0": [AnalogIn(ads=adc1, gain=1, positive_pin=ADS.P0)],
+                "1A1": [AnalogIn(ads=adc1, gain=1, positive_pin=ADS.P1)],
+                "1A2": [AnalogIn(ads=adc1, gain=1, positive_pin=ADS.P2)],
+                "1A3": [AnalogIn(ads=adc1, gain=1, positive_pin=ADS.P3)]
+            },
+            "2AX": {
+                "2A0": [AnalogIn(ads=adc2, gain=1, positive_pin=ADS.P0)],
+                "2A1": [AnalogIn(ads=adc2, gain=1, positive_pin=ADS.P1)],
+                "2A2": [AnalogIn(ads=adc2, gain=1, positive_pin=ADS.P2)],
+                "2A3": [AnalogIn(ads=adc2, gain=1, positive_pin=ADS.P3)]
+            },
+            "3AX": {
+                "3A0": [AnalogIn(ads=adc3, gain=1, positive_pin=ADS.P0)],
+                "3A1": [AnalogIn(ads=adc3, gain=1, positive_pin=ADS.P1)],
+                "3A2": [AnalogIn(ads=adc3, gain=1, positive_pin=ADS.P2)],
+                "3A3": [AnalogIn(ads=adc3, gain=1, positive_pin=ADS.P3)]
+            }
+        }
+
+
+pins = []
+for pin in adcPinMap["0AX"].values():
+    pins.append(pin)
+
+
+startTime = time.perf_counter()
+while True:
+    for pin in pins:
+        timeElapsed = time.perf_counter() - startTime
+        print("{}: {}".format(timeElapsed,pin[0].value))
