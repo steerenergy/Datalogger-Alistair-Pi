993eaab68bcb8e52ffcbd1c920c41b7a74ee3f45
Alistair-L-R <alistairlryan@outlook.com>
2021-07-16 17:18:51 +0100

    Edited ads1x15.py to make sure gain is configured for each pin when logging

--- adafruit_ads1x15/analog_in.py
+++ adafruit_ads1x15/analog_in.py
@@ -45,7 +45,7 @@ class AnalogIn:
     def value(self):
         """Returns the value of an ADC pin as an integer."""
         return self._ads.read(
-            self._pin_setting, is_differential=self.is_differential
+            self._pin_setting, is_differential=self.is_differential, gain=self._gain
         ) << (16 - self._ads.bits)
 
     @property
