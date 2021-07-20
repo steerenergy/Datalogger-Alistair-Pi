1492ffef370046488a6af4fd3e1810fb8288847f
Alistair-L-R <alistairlryan@outlook.com>
2021-07-20 10:07:28 +0100

    Making TCP disconnect actually close connection

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ if __name__ == '__main__':
     #mp.set_start_method('spawn')
     print("Starting application.")
     connGui, connTcp = Pipe()
-    exitTcp = Event()
+    #exitTcp = Event()
     # Create thread for TCP server to run on
     # This is so TCP connections can be processed separate from the GUI
-    serverThread = Thread(target=tcp.run, args=(connTcp,exitTcp))
+    #serverThread = Thread(target=tcp.run, args=(connTcp,exitTcp))
+    serverThread = Thread(target=tcp.run, args=(connTcp,))
     serverThread.daemon = True
     serverThread.start()
     print("ServerThread starting")
@@ -25,4 +26,5 @@ if __name__ == '__main__':
     # logger = logPi.Logger()
     # logger.run()
     # Start the GUI
-    gui.run(connGui,exitTcp)
+    #gui.run(connGui,exitTcp)
+    gui.run(connGui)
