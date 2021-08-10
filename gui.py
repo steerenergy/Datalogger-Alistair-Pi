9b1ea35d20ba4c565d5404dcfd0906dc9a74d47b
Alistair-L-R <alistairlryan@outlook.com>
2021-08-10 14:38:54 +0100

    Removed time sleep

--- gui.py
+++ gui.py
@@ -349,7 +349,7 @@ class WindowTop(Frame):
                         timeData = timeData[-1000:]
                         for i in range(0, len(logData)):
                             logData[i] = logData[i][-1000:]
-            time.sleep(0.01)
+            #time.sleep(0.01)
 
 
     def commandHandler(self, connGui):
