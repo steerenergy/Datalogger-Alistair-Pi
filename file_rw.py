8f46bdd83c5fefb31d26c248cd1294b611870ea9
Alistair-L-R <alistairlryan@outlook.com>
2021-08-18 16:23:18 +0100

    Disallow running on individual scripts without running main.py

--- file_rw.py
+++ file_rw.py
@@ -103,3 +103,11 @@ def GetSize(path):
             lineNum += 1
             line = file.readline()
     return lineNum
+
+# This is the code that is run when the program is loaded.
+# If the module were to be imported, the code inside the if statement would not run.
+if __name__ == "__main__":
+    # Warning that logger will not work
+    print("\nWARNING - This script cannot be run directly."
+          "\nPlease run 'main.py' to start the logger, or use the desktop icon.\n")
+    # Script will exit
--- logObjects.py
+++ logObjects.py
@@ -54,3 +54,12 @@ class LogMeta():
         for pin in self.config:
             if pin.enabled:
                 self.enabled += 1
+
+
+# This is the code that is run when the program is loaded.
+# If the module were to be imported, the code inside the if statement would not run.
+if __name__ == "__main__":
+    # Warning that logger will not work
+    print("\nWARNING - This script cannot be run directly."
+          "\nPlease run 'main.py' to start the logger, or use the desktop icon.\n")
+    # Script will exit
